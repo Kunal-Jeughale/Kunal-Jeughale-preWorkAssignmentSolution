e1432b8dfbff177f515ceac2630a1c05c45f9694
Kunal-Jeughale <111232049+Kunal-Jeughale@users.noreply.github.com>
2022-08-21 20:16:30 +0530

    Add files via upload

---
+++ Fibonacciseries.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Fibonacciseries {
+public static void main(String[] args) {
+	int i, n1=0, n2=1,n3, count;
+	System.out.print("Enter a number");
+	Scanner Sc= new Scanner(System.in);
+	count=Sc.nextInt();
+	System.out.print(n1+" "+n2);
+	for(i=2; i<count;i++) {
+		n3=n1+n2;
+		System.out.print(" "+ n3);
+		n1=n2;
+		n2=n3;
+	}
+	
+}
+}
